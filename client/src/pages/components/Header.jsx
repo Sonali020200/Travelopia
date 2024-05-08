222b96af422bfa0cd6f78abdce169560a701cb72
Sonali Burman <sonali.b.020200@gmail.com>
2024-05-08 18:25:50 +0530

    frontend

--- client/src/pages/components/Header.jsx
+++ client/src/pages/components/Header.jsx
@@ -8,24 +8,11 @@ const Header = () => {
   return (
     <>
       <div className="bg-slate-400 p-4 flex justify-between items-center">
-        <h1
-          className="h-min text-4xl font-bold relative"
-          style={{
-            color: "transparent",
-            WebkitTextStroke: "0.7px",
-            WebkitTextStrokeColor: "#fff",
-          }}
-        >
-          Come
-          <span
-            className="shadow-xl rounded-lg text-slate-700 text-2xl absolute left-1 top-[-10px] text-center"
-            style={{
-              WebkitTextStroke: "0",
-            }}
-          >
-            Dream Tours
-          </span>
-        </h1>
+      
+      <img src="https://www.leopardcatamaransbrokerage.com/sites/leopard/files/images/travelopia-logo.jpg" alt="Logo" className="h-16 w-auto mr-2" />
+
+
+
         <ul className="flex flex-wrap items-center justify-end gap-2 text-white font-semibold list-none">
           <li className="hover:underline hover:scale-105 transition-all duration-150">
             <Link to={`/`}>Home</Link>
